6d5bc7677633e1b99996d71334107c6cf89892dd
Shobhan S <shobhan.11.2000@gmail.com>
2022-03-28 17:37:49 +0530

    Pytest training is deployed.

--- test_purchase.py
+++ test_purchase.py
@@ -1,7 +1,15 @@
 import pytest
 
-def test_AddItemtoCart():
+@pytest.fixture
+def SetUp():
+    print("Setup started")
+
+    yield
+    print("Exited!!")
+
+
+def test_AddItemtoCart(SetUp):
     print("Added successfully.")
 
-def test_RemoveItemfromCart():
+def test_RemoveItemfromCart(SetUp):
     print("Removed successfully.")
